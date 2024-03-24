ec02ed4bedf1070024ecff3e43f4897bca638a49
Bast Bašista <basissar@fel.cvut.cz>
2024-03-24 09:48:46 +0100

    Edited authorization middleware

--- backend/src/controller/authorization.ts
+++ backend/src/controller/authorization.ts
@@ -1,13 +1,14 @@
 import {RouterContext} from 'https://deno.land/x/oak@v12.6.1/router.ts';
 import { key } from "../utils/apiKey.ts";
-import { create, getNumericDate, verify } from "https://deno.land/x/djwt@v2.4/mod.ts";
+import { verify } from "https://deno.land/x/djwt@v2.4/mod.ts";
+import { FORBIDDEN, UNAUTHORIZED } from '../config/macros.ts';
 
 const authorization = async (ctx: RouterContext<string>, next: () => Promise<any>) => {
     try {
         const token = ctx.request.headers.get('Authorization')?.split(' ')[1];
 
         if (!token) {
-            ctx.response.status = 401;
+            ctx.response.status = UNAUTHORIZED;
             ctx.response.body = { message: 'Authorization token missing' };
             return;
         }
@@ -15,7 +16,7 @@ const authorization = async (ctx: RouterContext<string>, next: () => Promise<any
         const result = await verify(token, key);
     
         if (!result) {
-            ctx.response.status = 401;
+            ctx.response.status = UNAUTHORIZED;
             ctx.response.body = { message: 'Invalid token or expired' };
             return;
         }
@@ -23,16 +24,19 @@ const authorization = async (ctx: RouterContext<string>, next: () => Promise<any
         const tokenId = (result as { payload: { id: number } }).payload.id;
     
         const paramsId = ctx.params.userId;
-    
-        if(tokenId != Number(paramsId)) {
-            ctx.response.status = 403;
-            ctx.response.body = { message: 'You are not authorized to perform this action' };
-            return;
+
+        if (Object.keys(paramsId).length !== 0) {
+
+            if(tokenId != Number(paramsId)) {
+                ctx.response.status = FORBIDDEN;
+                ctx.response.body = { message: 'You are not authorized to perform this action' };
+                return;
+            }
         }
     
         await next();
     } catch (err) {
-        ctx.response.status = 401;
+        ctx.response.status = UNAUTHORIZED;
         ctx.response.body = { message: 'Authorization failed due to an error: ' + err.message };
     }
     
