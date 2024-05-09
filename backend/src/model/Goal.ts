573ba008ad4000ce8dab560cbc9d962587c87df2
Bast Bašista <basissar@fel.cvut.cz>
2024-05-10 00:13:42 +0200

    edited goal model

--- backend/src/model/Goal.ts
+++ backend/src/model/Goal.ts
@@ -6,13 +6,17 @@ import { Wallet } from './Wallet.ts'
 export class Goal extends Model{
 
     @Column({allowNull: false})
+    declare name: string;
+
+    @Column({allowNull: false})
     declare targetAmount: number;
 
     @Column({allowNull: false})
     declare currentAmount: number;
 
+    @ForeignKey(() => Category)
     @Column({allowNull: false})
-    declare deadline: Date;
+    declare categoryId: number;
 
     @ForeignKey(() => Wallet)
     @Column({
@@ -22,4 +26,6 @@ export class Goal extends Model{
     })
     declare walletId: string;
 
+    @BelongsTo(() => Category, { foreignKey: 'categoryId', as: 'category' })
+    declare category: Category;
 }
