f9f23d4fa6711705bfeaf727729975536fe96f54
Bast Bašista <basissar@fel.cvut.cz>
2024-05-20 02:04:52 +0200

    Database file edit

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -7,6 +7,12 @@ import { Wallet } from '../model/Wallet.ts';
 import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
 import { Expense } from '../model/Expense.ts';
 import pg from 'npm:pg';
+import { Account } from "../model/Account.ts";
+import { Avatar } from "../model/Avatar.ts";
+import { Item } from "../model/Item.ts";
+import { ItemAvatar } from "../model/ItemAvatar.ts";
+import { ItemEquipped } from "../model/ItemEquipped.ts";
+import { ItemOwned } from "../model/ItemOwned.ts";
 
 config({ export: true });
 
@@ -28,11 +34,22 @@ export const sequelize = new Sequelize({
     }
 });
 
+// const sequelize = new Sequelize({
+//     database: 'postgres', 
+//     username: 'postgres',
+//     password: 'password',
+//     host: 'localhost',
+//     port: 5432,
+//     dialect: 'postgres',
+//     dialectModule: pg,
+//     models: [Account, User, Wallet, Expense, Budget, Goal, Category, Avatar, Item, ItemOwned, ItemEquipped, ItemAvatar],
+//   });
+
 export async function initializeDatabase() {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
-        await sequelize.addModels([User, Wallet, Category, Budget, Goal, Expense]);
+        await sequelize.addModels([Account, User, Wallet, Expense, Budget, Goal, Category, Avatar, Item, ItemOwned, ItemEquipped, ItemAvatar]);
         // await sequelize.sync({force: true});
         await sequelize.sync();
         console.log('Models synchronized successfully.');
